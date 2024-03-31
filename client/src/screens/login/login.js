c5de085103898589afdc691cf444c11dd6824c99
hakim <hdebiche@gmail.com>
2024-03-31 03:49:36 +0100

    creation de l'api de connexion

--- client/src/screens/login/login.js
+++ client/src/screens/login/login.js
@@ -3,11 +3,18 @@ import './login.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "bootstrap-icons/font/bootstrap-icons.css";
 import loginImage from './../../assets/Login2.png';
+import axios from 'axios'
+import ErreurMessage from './../../components/message_erreur/ErreurMessage'
 
 
 function Login({setShowLogin, showLogin}) {
 
     const [aria, setAria] = useState(false)
+
+    const [email, setEmail] = useState('')
+    const [mdp, setMdp] = useState('')
+
+    const [erreur, setErreur] = useState('')
 
     const fermer = () => {
         setAria(true);
@@ -16,6 +23,32 @@ function Login({setShowLogin, showLogin}) {
             setShowLogin(false);
             setAria(false)
           }, 200);
+    }
+
+    const login = (e) => {
+        e.preventDefault();
+        //console.log(email + "    " + mdp)
+
+        //verifier si les champs ne sont pas vides
+        if(email === '' || mdp === ''){
+            setErreur('Veuillez renseigner vos identifiants')
+            return;
+        }
+
+        axios.post("http://localhost:4000/auth/login", {email, mdp}) 
+        .then(res => {
+          if(res.data.statut === "erreur"){
+            setErreur(res.data.message)
+            //console.log(res);
+          } else {
+            console.log(res.data.statut)
+            //navigate('/'); naviguer vers le dashboard de l'assure
+          }
+        })
+        .catch(err => {
+          setErreur("Une erreur est survenu, réessayez plus tard")
+          //console.log(err)
+        })
     }
 
   return (
@@ -43,31 +76,38 @@ function Login({setShowLogin, showLogin}) {
                 {/* <!-------------------- ------ Right Box ----------------------------> */}
                 <div className="col-md-6 right-box">
                     <div className="row align-items-center">
-                            <div className="header-text mb-2">
+                            <div className="header-text">
                                 <h2 className='mb-0'>Se connecter</h2>
                                 <p className=''>Nous sommes heureux de vous revoir.</p>
                             </div>
-                            <div className="form-floating mb-3">
-                                <input type="email" className="form-control" id="email" placeholder="name@example.com"/>
-                                <label htmlFor="email" className='label'>Adresse Email</label>
-                            </div>
-                            <div className="form-floating mb-1">
-                                <input type="password" className="form-control" id="mdp" placeholder="Mot de Passe" />
-                                <label htmlFor="mdp" className='label'>Mot de Passe</label>
-                            </div>
-                            
-                            <div className="input-group mb-4 d-flex justify-content-between">
-                                <div className="form-check">
-                                    <input type="checkbox" className="form-check-input" id="formCheck"/>
-                                    <label htmlFor="formCheck" className="form-check-label text-secondary"><small>Se souvenir de moi</small></label>
+
+                            {erreur !== '' && <ErreurMessage message={erreur} />}
+
+                            <form onSubmit={login}>
+                                <div className="form-floating mb-3">
+                                    <input type="email" className="form-control" id="email" placeholder="hdeb@gmail.com" 
+                                    onChange={(e) => setEmail(e.target.value)}/>
+                                    <label htmlFor="email" className='label'>Adresse Email</label>
                                 </div>
-                                <div className="forgot">
-                                    <small><a href='h'>Mot de passe oublié ?</a></small>
+                                <div className="form-floating mb-1">
+                                    <input type="password" className="form-control" id="mdp" placeholder="mot de passe" 
+                                    onChange={(e) => setMdp(e.target.value)}/>
+                                    <label htmlFor="mdp" className='label'>Mot de Passe</label>
                                 </div>
-                            </div>
-                            <div className="input-group mb-2 mt-2">
-                                <button className="btn btn-lg btn-primary w-100 fs-6">Login</button>
-                            </div>
+                                
+                                <div className="input-group mb-4 d-flex justify-content-between">
+                                    <div className="form-check">
+                                        <input type="checkbox" className="form-check-input" id="formCheck"/>
+                                        <label htmlFor="formCheck" className="form-check-label text-secondary"><small>Se souvenir de moi</small></label>
+                                    </div>
+                                    <div className="forgot">
+                                        <small><a href='h'>Mot de passe oublié ?</a></small>
+                                    </div>
+                                </div>
+                                <div className="input-group mb-2 mt-2">
+                                    <button type='submit' className="btn btn-lg btn-primary w-100 fs-6">Login</button>
+                                </div>
+                            </form>
                             
                             <div className="row">
                                 <small>Si vous n'avez pas de compte <a href="h">contactez</a> la CASNOS</small>
