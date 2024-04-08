c83a4ba69985f919fb87af395165f5df8e31b3cd
brahim <brahim13khalil@gmail.com>
2024-04-08 02:17:49 +0100

    working on rendering the pdf

--- client/src/components/recours_controllers/Message_valide.jsx
+++ client/src/components/recours_controllers/Message_valide.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import AccuserPdf from "./AccuserPdf";
 
 const Message_valide = () => {
   return (
@@ -8,6 +9,7 @@ const Message_valide = () => {
       <div className="message">
         <i class="bi bi-check-circle-fill"></i>
         <h4>Recours ajoute avec succes</h4>
+        <AccuserPdf />
         <Link to="/assure/tableau_de_bord" className="lien_tablau">
           Exit
         </Link>
